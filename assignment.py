c10ba0eca14271767be1c0345c592631b91a7997
OstohMichyy <osteen12024@gmail.com>
2026-03-12 23:52:11 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,20 @@
+# Question 4
+print("Hello AI Students")
+# Question 5
+name = "Osteen"
+age = 18
+city = "Mombasa"
 
+print(name)
+print(age)
+print(city)
+# Question 6
+languages = ["Java", "C++", "JavaScript", "Python" ]
+print(languages[0])
+# Question 7
+student = {
+    "name" : "Osteen",
+    "age" : 18,
+    "course" : "AI"
+}
+print(student)
